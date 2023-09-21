36a97ae9ffa42f8c06fda3f0f648e9508d115c80
Alyanna Gesmundo <monokaisekai@gmail.com>
2023-09-21 18:49:44 +0800

    add sample repo and modify button

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Add a click event listener to the button
     button.addEventListener('click', function() {
-        const urlToOpen = 'https://github.com/monokaisekai/hello';
+        const urlToOpen = 'https://monokaisekai.github.io/hello/hello.html';
         
         // Open the URL in a new tab
         window.open(urlToOpen, '_blank');
